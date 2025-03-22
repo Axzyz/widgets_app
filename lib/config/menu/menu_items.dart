f7b7bc001976d33996e2f1544c9e8e395baec2e9
Axzyz <ronald.urrutia.vera@gmail.com>
2025-03-22 20:29:47 -0300

    Pantalla progress_screen finalizada
    Se finaliza la pantalla de progress indicators

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -27,4 +27,10 @@ const appMenuItems = <MenuItem>[
     link: '/cards',
     icon: Icons.credit_card,
   ),
+  MenuItem(
+    title: 'Indicadores de Progreso',
+    subTitle: 'Barras de progreso generales y controladas',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
+  ),
 ];
